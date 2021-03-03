0326d802928e08f92c8f3a86ca40abee478a3519
Fernando Azevedo <azevedo-fernando@users.noreply.github.com>
2021-03-03 16:58:25 -0300

    Iniciando o Cadastro de usuários

---
+++ src/routes/users.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const routes = express.Router() 
+
+// const SessionController = require('../app/controllers/SessionController')
+const UserController = require('../app/controllers/UserController')
+
+// // LOGIN / LOGOUT
+// routes.get('/login', SessionController.loginForm)
+// routes.post('/login', SessionController.login)
+// routes.post('/logout', SessionController.logout)
+
+// //reset PASSWORD / FORGOT
+// routes.get('/forgot-password', SessionController.forgotForm)
+// routes.get('/password-reset', SessionController.resetForm)
+// routes.post('/forgot-password', SessionController.forgot)
+// routes.post('/password-reset', SessionController.reset)
+
+// // USER REGISTER UserController
+routes.get('/register', UserController.registerForm)
+// routes.post('/register', UserController.post)
+
+// routes.post('/', UserController.show)
+// routes.put('/', UserController.update)
+// routes.delete('/', UserController.delete)
+
+module.exports = routes
